7ed98f64f74809db9bd0fe90fc8fc25a4b065631
Yurii <yuri_n@gmaill.com>
2024-05-07 18:27:00 +0300

    homework 10.2 added

---
+++ dz102.js
@@ -0,0 +1,35 @@
+/*У нас параноя, ми не довіряємо жодним адресам, окрім зареєстрованих на доменах gmail.com та yahoo.com.
+За допомогою регулярного виразу створіть масив з адресами, гідними довіри. Постарайтеся також зробити просту валідацію до @
+одне, або два слова які можуть містити (латинські букви, цифри) яке може бути розділене крапкою.*/
+
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+        },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+    ];
+
+var str1 = "test.te.st@gmail.com";
+var str2 = "dmitro.porohov@yahoo.com";
+var str3 = "andrii@mail.ru";
+
+var pattern = /^[a-zA-Z0-9]+\.?[a-zA-Z0-9]+@(yahoo.com|gmail.com)$/;
+
+
+for (let arrElement of arr) {
+    if (pattern.test(arrElement.email) === true) {
+        console.log(`${arrElement.email} is a VALID address`)
+    }
+    else {console.log(`${arrElement.email} is NOT a valid address`)}
+}
